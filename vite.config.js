646da59ac06d9320b3e6a0ffc7a6861e139968c7
Andrew_Time <34838425+shhh-hoo@users.noreply.github.com>
2026-01-06 17:26:22 +0800

    Add main visual page to Vite build (#50)

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => ({
         main: fileURLToPath(new URL("./index.html", import.meta.url)),
         wss2: fileURLToPath(new URL("./wss2.html", import.meta.url)),
         host: fileURLToPath(new URL("./host.html", import.meta.url)),
+        mainVisual: fileURLToPath(new URL("./main-visual.html", import.meta.url)),
       },
     },
   },
